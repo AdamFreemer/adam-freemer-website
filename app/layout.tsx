ed0c130130a5e165a783c57debd60635f31e9ad2
Adam Freemer <adam@MacBookProM1.local>
2025-06-26 09:56:38 -0400

    Update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Adam Freemer',
+  description: 'Software Engineer and Technologist',
 }
 
 export default function RootLayout({
